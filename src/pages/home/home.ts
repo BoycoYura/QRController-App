0221a4cee1389d116bb94ebcae7fd373657715a8
YuraBoyco <boyco-yura@yandex.ru>
2018-06-23 11:01:03 +0300

    add confirm ticket logic to project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,13 +11,21 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 })
 export class HomePage {
   options: BarcodeScannerOptions;
+
   encodText:string='';
+
   encodedData:any={};
+
   scanData:any= {};
+
   public reqStatus;
+
+  public BiletState;
 
   private apiUrl ='http://greenworld.by/api/check_ticket?access_token=';
   // private apiUrl ='/api/check_ticket?access_token=';
+
+  private apiCheck = '/api/ticket_confirm';
 
   constructor(public navCtrl: NavController, public scanner:BarcodeScanner, public menuCtrl: MenuController,private httpClient: HttpClient) {
     menuCtrl.enable(true);
@@ -54,6 +62,38 @@ export class HomePage {
       this.scanData = data;
 
       var access_token = JSON.parse( localStorage.getItem("usInfo")) ;
+
+      var formData = new FormData();
+      formData.append("ticket_id", this.scanData.text);
+      formData.append("access_token", access_token.token);
+  
+      this.httpClient.post(this.apiCheck,formData).subscribe(
+        res => {
+          console.log("Post status:")
+          console.log(res);
+
+          this.BiletState = res;
+
+          if(this.BiletState.state == 'used'){
+            this.reqStatus =  false;
+          }
+  
+          if(this.BiletState.state == 'new'){
+            this.reqStatus =  true;
+          }
+        },
+        err => {
+          var er_status = err.status;
+          alert(er_status);
+
+          if(er_status == '404'){
+            this.reqStatus =  false;
+          }
+  
+          if(er_status == '500'){
+            alert("Ошибка сервера");
+          }
+        });
 
       this.httpClient.get(this.apiUrl+access_token.token+"&"+"ticket_id="+this.scanData.text).subscribe(
         res => {
